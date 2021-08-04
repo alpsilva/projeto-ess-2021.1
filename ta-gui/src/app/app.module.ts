64e502c120eac9b1cfc48c4f6605fc5ba058ab36
André Luiz <alps2@cin.ufpe.br>
2021-08-04 12:03:24 -0300

    Adicionada tela do professor

--- ta-gui/src/app/app.module.ts
+++ ta-gui/src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule }   from '@angular/router';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TeacherComponent } from './teacher.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TeacherComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      {
+        path: 'teacher',
+        component: TeacherComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
