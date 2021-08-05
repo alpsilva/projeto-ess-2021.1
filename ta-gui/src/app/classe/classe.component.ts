013be5ae21798e2c87a7ea031db393486cc9c99f
André Luiz <alps2@cin.ufpe.br>
2021-08-05 11:59:05 -0300

    Página de uma turma (sem routing ainda)

--- ta-gui/src/app/classe/classe.component.ts
+++ ta-gui/src/app/classe/classe.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Turma } from '../../../../commons/turma';
+import { TurmaService } from '../turmas/turmas.service';
+import { Aluno } from '../../../../commons/aluno';
 
 @Component({
   selector: 'app-classe',
@@ -7,9 +10,105 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClasseComponent implements OnInit {
 
-  constructor() { }
+  turmaId: number = -1;
+  turma: Turma = new Turma();
+
+  title = this.turma.nome;
+
+  aluno: Aluno = new Aluno();
+  delCpf: string = "";
+
+  constructor(private turmaService: TurmaService) {}
+
+  // informações da turma
+
+  // lista de alunos
+  // adicionar/remover alunos
+  // botar notas nas metas dos alunos
+  
+  // adicionar metas para a turma
+  
+
+  criarTurma(t: Turma): void {
+    var result = this.turmaService.criar(t);
+    if (result){
+      this.turmas.push(result);
+      this.turma = new Turma();
+    }
+  }
+
+  deletarTurma(id: number): void {
+    var result = this.turmaService.deletar(id);
+    if (result){
+      // Procura o index do objeto td a ser deletado e usa o método splice para tirar ele do array.
+      for (let i = 0; i < this.turmas.length; i++){
+        if (this.turmas[i].id == result.id){
+          this.turmas.splice(i, 1);
+        }
+      }    
+    }
+  }
+
+  onMove(): void {
+    //pode vir a ter algo
+  }
 
   ngOnInit(): void {
+    this.turmaId = 
+    this.turma = this.turmaService.getOnlyTurma(this.turmaId);
+  }
+
+
+  // router para ir para a página da turma
+  goToTurma(id: number): void{
+    // algum tipo de lógica para redirecionar a página para o endereço da turma que possui identificação == id
   }
 
 }
+
+  // this will be used when we have a server
+
+  /*
+
+    criarTurma(t: Turma): void {
+      this.turmaService.criar(t)
+            .subscribe(
+              tr => {
+                if (tr) {
+                  this.turmas.push(tr);
+                  this.turma = new Turma();
+                }
+              },
+              msg => { alert(msg.message); }
+            );
+    }
+
+    deletarTurma(id: number): void {
+      this.turmaService.deletar(id).subscribe(
+        td => {
+          if (td) {
+            // Procura o index do objeto ad a ser deletado e usa o método splice para tirar ele do array.
+            for (let i = 0; i < this.turmas.length; i++){
+              if (this.turmas[i].id == td.id){
+                this.turmas.splice(i, 1);
+              }
+            }          
+          }
+        },
+        msg => { alert(msg.message); }
+      );
+    }
+
+    onMove(): void {
+      //pode vir a ter algo
+    }
+
+    ngOnInit(): void {
+      this.turmaService.getTurmas()
+            .subscribe(
+              ts => { this.turmas = ts; },
+              msg => { alert(msg.message); }
+            );
+    }
+
+  */
