f82ac6652c7b0a3a5a05cd29db96f073ef069e3b
André Luiz <alps2@cin.ufpe.br>
2021-08-12 22:02:27 -0300

    First tests about creating and removing classes
    Not fully working because we don't know how to handle browser alerts and confirms yet (general pop-ups)

--- tests-acceptance/compiledCode/config/config.js
+++ tests-acceptance/compiledCode/config/config.js
@@ -1,27 +1,20 @@
-import { browser, Config } from 'protractor';
-
-export let config: Config = {
-
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const protractor_1 = require("protractor");
+exports.config = {
     seleniumAddress: 'http://127.0.0.1:4444/wd/hub',
-
     SELENIUM_PROMISE_MANAGER: false,
-
     capabilities: {
         browserName: 'firefox'
     },
-
     framework: 'custom',
     frameworkPath: require.resolve('protractor-cucumber-framework'),
-
     specs: [
         '../../features/CRUD_class.feature'
     ],
-
     onPrepare: () => {
-
-        browser.ignoreSynchronization = true;
-        browser.manage().window().maximize();
-
+        protractor_1.browser.ignoreSynchronization = true;
+        protractor_1.browser.manage().window().maximize();
     },
     cucumberOpts: {
         compiler: "ts:ts-node/register",
--- tests-acceptance/stepdefinitions/classScenario_1.ts
+++ tests-acceptance/stepdefinitions/classScenario_1.ts
@@ -1,43 +1,66 @@
 import { defineSupportCode } from 'cucumber';
 import { browser, $, element, ElementArrayFinder, by } from 'protractor';
+let chai = require('chai').use(require('chai-as-promised'));
+let expect = chai.expect;
 
-defineSupportCode(function ({ GIVEN, WHEN, THEN }) {
-    GIVEN(/^I am already logged in my teacher level account with login “MarDam” and password “21@Dam#20”$/, async() => {
+let sameTurmaName = ((elem, name) => elem.element(by.name('turmaNomeList')).getText().then(text => text === name));
+
+defineSupportCode(function ({ Given, When, Then }) {
+    Given(/^I am already logged in as a teacher$/, async() => {
         await browser.get("http://localhost:4200/");
         await expect(browser.getTitle()).to.eventually.equal('TaGui');
         await $("a[name='teacherBtn']").click();
     
-    })
-    GIVEN(/^I am in the classes page$/, async() => {
+    });
+
+    Given(/^I am in the classes page$/, async() => {
         await $("a[name='classesBtn']").click();
-    })
-    WHEN(/I try to create a new class^$/, async() => {
+    });
 
-    })
-    WHEN(/^I fill the class name with “ESS 2021.1 - Turma 1$/, async() => {
+    When(/^I try to create a new class with the name “([^\"]*)” and description “([^\"]*)”$/, async(nomeTurma, descricao) => {
+        await $("input[name='turmaNameBox']").sendKeys(<string>nomeTurma);
+        await $("input[name='turmaDescriptionBox']").sendKeys(<string>descricao);
+        await element(by.buttonText('Adicionar')).click();
+    });
 
-    })
-    WHEN(/^I fill the description with “Turma de Engenharia de Software e Sistemas do semestre letivo de 2021.1”$/, async() => {
+    Then(/^I now see the class with the name “([^\"]*)” in the list of registered classes$/, async(nomeTurma) => {
+        var allTurmas : ElementArrayFinder = element.all(by.name('turmaList'));
+        await allTurmas.filter(elem => sameTurmaName(elem,nomeTurma)).then
+        (elems => expect(Promise.resolve(elems.length)).to.eventually.equal(1));
+    });
 
-    })
-    THEN(/^I see a message informing me  that it was registered successfully$/, async() => {
-        
-    })
-    THEN(/^I now see the class with the name “ESS 2021.1 - Turma 1” in the list of registered class$/, async() => {
+    Given(/^I see the class with the name “([^\"]*)” in the list of registered classes$/, async(nomeTurma) => {
+        await $("input[name='turmaNameBox']").sendKeys(<string>nomeTurma);
+        await $("input[name='turmaDescriptionBox']").sendKeys(<string>"");
+        await element(by.buttonText('Adicionar')).click();
+        var allTurmas : ElementArrayFinder = element.all(by.name('turmaList'));
+        await allTurmas.filter(elem => sameTurmaName(elem,nomeTurma)).then
+        (elems => expect(Promise.resolve(elems.length)).to.eventually.equal(1));
+    });
 
-    })
-    WHEN(/^ $/, async() => {
-        
-    })
-    WHEN(/^ $/, async() => {
-        
-    })
-    WHEN(/^ $/, async() => {
-        
-    })
-    WHEN(/^ $/, async() => {
-        
-    })
+    // delete
+    When(/^I try to delete the class with the name “([^\"]*)”$/, async(nomeTurma) => {
+        await element(by.buttonText('Deletar Turma: ' + nomeTurma)).click();
+    });
 
+    When(/^I say ok to the pop-up asking to confirm it$/, async(nomeTurma) => {
+        // lidar com o confirm
+    });
 
-}
+    Then(/^I can no longer see a class named “([^\"]*)” in the list of registered classes$/, async(nomeTurma) => {
+        var allTurmas : ElementArrayFinder = element.all(by.name('turmaList'));
+        await allTurmas.filter(elem => sameTurmaName(elem,nomeTurma)).then
+        (elems => expect(Promise.resolve(elems.length)).to.eventually.equal(0));
+    });
+
+    Then(/^I see a message informing me that it was not registered because there is already a class with that name$/, async(nomeTurma) => {
+        // lidar com o alert
+    });
+
+    Then(/^I see a message informing me that it was not registered because there is already 3 classes registered$/, async(nomeTurma) => {
+        //lidar com o alert
+    });
+
+    
+
+});
