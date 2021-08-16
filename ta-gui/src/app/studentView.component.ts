6e2bf9efada546c27b6d51ffee8d8df1e03c5d10
André Luiz <alps2@cin.ufpe.br>
2021-08-16 14:30:43 -0300

    Versão final do studentView

--- ta-gui/src/app/studentView.component.ts
+++ ta-gui/src/app/studentView.component.ts
@@ -13,37 +13,46 @@ import { TurmaService } from './turmas/turmas.service';
 export class StudentViewComponent implements OnInit {
   title = 'Aluno';
 
-  studentCpf:string = "-1";
+  studentCpf:string = "";
   turmas: Turma[] = [];
+  grades: Map<number, Map<string, string>> = new Map<number, Map<string, string>>();
 
   constructor(private turmaService: TurmaService) {}
 
   getAllEnrolledClasses(cpf: string): void {
+    this.turmas = [];
     this.turmaService.getTurmas().subscribe(
-        allTurmas => {
-          if (allTurmas) {
-              for (let t of allTurmas){
-                  for (let a of t.getAlunos()){
-                      if (a.cpf == this.studentCpf){
-                        this.turmaService.getMetasOf(t.id, a).subscribe(
-                            metas => {
-                              console.log(metas);
-                              for (let m of metas) {
-                                a.metas.set(m[0], m[1]);
-                              }
-                            },
-                            msg => {console.log(msg.message);}
-                          );
-                          this.turmas.push(t);
-                      }
-                  }
-              }           
-          } else {
-            //server POST failure msg
-          }
-        },
-        msg => { msg.alert }
-      );
+      allTurmas => {
+        if (allTurmas) {
+            for (let t of allTurmas){
+              for (let a of t.alunoLista.alunos){
+                  if (a.cpf == this.studentCpf){
+                    this.turmas.push(t);
+                    let metas: Map<string, string> = new Map<string, string>();
+                    this.turmaService.getMetasOf(t.id, a).subscribe(
+                      ms => {
+                        for (let m of ms) {
+                          metas.set(m[0], m[1]);
+                        }
+                        this.grades.set(t.id, metas);
+                      },
+                      msg => {console.log(msg.message);}
+                    );
+                  }   
+              }
+            }           
+        } else {
+          //server POST failure msg
+        }
+      },
+      msg => { msg.alert }
+    );
+  }
+
+  getGrade(id: number, meta: string): string {
+    var metas: Map<string, string> = new Map<string, string>(this.grades.get(id));
+    var str = metas.get(meta);
+    return str;
   }
 
   onMove(): void {
