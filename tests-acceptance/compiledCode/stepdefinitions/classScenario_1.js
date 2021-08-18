954ace24338f422a89277e0000ee0bf0dd06586b
pietromasur <pietromasur@hotmail.com>
2021-08-18 12:32:38 -0300

    Removendo açoes de alerta dos testes automatizados

--- tests-acceptance/compiledCode/stepdefinitions/classScenario_1.js
+++ tests-acceptance/compiledCode/stepdefinitions/classScenario_1.js
@@ -13,12 +13,18 @@ const protractor_1 = require("protractor");
 require("selenium-webdriver");
 let chai = require('chai').use(require('chai-as-promised'));
 let expect = chai.expect;
-let sameTurmaName = ((elem, name) => elem.element(protractor_1.by.name('turmaNomeList')).getText().then(text => text === name));
-let sameAlunoName = ((elem, name) => elem.element(protractor_1.by.name('alunolist')).getText().then(text => text === name));
+//Declaração do WebDriver (usado para lidar com alertas)
 const { Builder, By, until } = require('selenium-webdriver');
 let driver = new Builder()
     .forBrowser('firefox')
     .build();
+//Função para turma
+let sameTurmaName = ((elem, name) => elem.element(protractor_1.by.name('turmaNomeList')).getText().then(text => text === name));
+//Funções para alunos
+let sameAlunoName = ((elem, name) => elem.element(protractor_1.by.name('nomelist')).getText().then(text => text === name));
+let sameAlunoCpf = ((elem, name) => elem.element(protractor_1.by.name('cpflist')).getText().then(text => text === name));
+let sameAlunoEmail = ((elem, name) => elem.element(protractor_1.by.name('emaillist')).getText().then(text => text === name));
+let sameAlunoGithub = ((elem, name) => elem.element(protractor_1.by.name('githublist')).getText().then(text => text === name));
 cucumber_1.defineSupportCode(function ({ Given, When, Then }) {
     Given(/^I am already logged in as a teacher$/, () => __awaiter(this, void 0, void 0, function* () {
         yield protractor_1.browser.get("http://localhost:4200/");
@@ -67,17 +73,47 @@ cucumber_1.defineSupportCode(function ({ Given, When, Then }) {
     Given(/^I am at the “([^\"]*)” class detailed page$/, (nomeTurma) => __awaiter(this, void 0, void 0, function* () {
         yield protractor_1.element(protractor_1.by.buttonText('Acessar Turma ' + nomeTurma)).click();
     }));
-    Given(/^I see "“([^\"]*)”, “([^\"]*)”, “([^\"]*)” in the students list$/, (a1, a2, a3) => __awaiter(this, void 0, void 0, function* () {
+    When(/^I add a new student with the name “([^\"]*)”, cpf “([^\"]*)”, e-mail “([^\"]*)” and github “([^\"]*)”$/, (nome, cpf, email, git) => __awaiter(this, void 0, void 0, function* () {
         yield protractor_1.$("a[name='alunos']").click();
-        //Simplificado para somente adicionar aluno
-    }));
-    When(/^I add a new student with the name “([^\"]*)”$/, (nome, cpf, email, git) => __awaiter(this, void 0, void 0, function* () {
         yield protractor_1.$("input[name='namebox']").sendKeys(nome);
         yield protractor_1.$("input[name='cpfbox']").sendKeys(cpf);
         yield protractor_1.$("input[name='mailBox']").sendKeys(email);
         yield protractor_1.$("input[name='githubbox']").sendKeys(git);
         yield protractor_1.element(protractor_1.by.buttonText('Adicionar')).click();
     }));
+    Then(/^I see a student named “([^\"]*)”, with cpf “([^\"]*)”, e-mail “([^\"]*)” and github “([^\"]*)” in the students list$/, (name, cpf, email, git) => __awaiter(this, void 0, void 0, function* () {
+        var allNames = protractor_1.element.all(protractor_1.by.name('nomelist'));
+        var allCpf = protractor_1.element.all(protractor_1.by.name('cpflist'));
+        var allEmail = protractor_1.element.all(protractor_1.by.name('email'));
+        var allGithub = protractor_1.element.all(protractor_1.by.name('githublist'));
+        yield allNames.filter(elem => sameAlunoName(elem, name)).then(elems => expect(Promise.resolve(elems.length)).to.eventually.equal(1));
+        yield allCpf.filter(elem => sameAlunoCpf(elem, cpf)).then(elems => expect(Promise.resolve(elems.length)).to.eventually.equal(1));
+        yield allEmail.filter(elem => sameAlunoEmail(elem, email)).then(elems => expect(Promise.resolve(elems.length)).to.eventually.equal(1));
+        yield allGithub.filter(elem => sameAlunoGithub(elem, git)).then(elems => expect(Promise.resolve(elems.length)).to.eventually.equal(1));
+    }));
     Then(/^I can see “([^\"]*)” and “([^\"]*)” in the students list$/, (a1, a2) => __awaiter(this, void 0, void 0, function* () {
+        var allNames = protractor_1.element.all(protractor_1.by.name('nomelist'));
+        yield allNames.filter(elem => sameAlunoName(elem, a1)).then(elems => expect(Promise.resolve(elems.length)).to.eventually.equal(1));
+        ;
+        yield allNames.filter(elem => sameAlunoName(elem, a2)).then(elems => expect(Promise.resolve(elems.length)).to.eventually.equal(1));
+        ;
+    }));
+    Then(/^I enter the learning goals page$/, () => __awaiter(this, void 0, void 0, function* () {
+        yield protractor_1.$("a[name='metas']").click();
+    }));
+    When(/^I register the following learning goals: “([^\"]*)”, “([^\"]*)” and “([^\"]*)”$/, (m1, m2, m3) => __awaiter(this, void 0, void 0, function* () {
+        yield protractor_1.$("input[name='novaMetaBox']").sendKeys(m1);
+        yield protractor_1.element(protractor_1.by.buttonText('Adicionar Meta')).click();
+        yield protractor_1.$("input[name='novaMetaBox']").sendKeys(m2);
+        yield protractor_1.element(protractor_1.by.buttonText('Adicionar Meta')).click();
+        yield protractor_1.$("input[name='novaMetaBox']").sendKeys(m3);
+        yield protractor_1.element(protractor_1.by.buttonText('Adicionar Meta')).click();
+    }));
+    Then(/^I can now see that the learning goals of this class are “([^\"]*)”, “([^\"]*)” and “([^\"]*)”$/, (m1, m2, m3) => __awaiter(this, void 0, void 0, function* () {
+        //verificar as metas
+        var allNames = protractor_1.element.all(protractor_1.by.name('nomelist'));
+    }));
+    Then(/^it is now possible for me to give each student their goal grade$/, () => __awaiter(this, void 0, void 0, function* () {
+        //verificar existencia de box
     }));
 });
